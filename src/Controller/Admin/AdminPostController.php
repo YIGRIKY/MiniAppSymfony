698ce635722b09fe7137fb68bb765065fa6a5c12
Egor Agafonov <mainmail.agaf@yandex.ru>
2020-11-30 16:45:43 +0300

    lesson 10

--- src/Controller/Admin/AdminPostController.php
+++ src/Controller/Admin/AdminPostController.php
@@ -3,25 +3,27 @@
 
 namespace App\Controller\Admin;
 
+
 use App\Entity\Category;
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Service\FileManagerServiceInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class AdminPostController extends AdminBaseController
 {
-
     /**
-     * @Route ("/admin/post", name="admin_post")
+     * @Route("/admin/post", name="admin_post")
      */
     public function index()
     {
         $post = $this->getDoctrine()->getRepository(Post::class)
             ->findAll();
         $checkCategory = $this->getDoctrine()->getRepository(Category::class)
-        ->findAll();
+            ->findAll();
         $forRender = parent::renderDefault();
         $forRender['title'] = 'Посты';
         $forRender['post'] = $post;
@@ -30,17 +32,23 @@ class AdminPostController extends AdminBaseController
     }
 
     /**
-     * @Route ("/admin/post/create", name="admin_post_create")
+     * @Route("/admin/post/create", name="admin_post_create")
      * @param Request $request
+     * @param FileManagerServiceInterface $fileManagerService
+     * @return RedirectResponse|Response
      */
-    public function create(Request $request)
+    public function create(Request $request, FileManagerServiceInterface $fileManagerService)
     {
         $em = $this->getDoctrine()->getManager();
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()){
+            $image = $form->get('image')->getData();
+            if ($image){
+                $fileName = $fileManagerService->imagePostUpload($image);
+                $post->setImage($fileName);
+            }
             $post->setCreateAtValue();
             $post->setUpdateAtValue();
             $post->setIsPublished();
@@ -55,36 +63,52 @@ class AdminPostController extends AdminBaseController
         return $this->render('admin/post/form.html.twig', $forRender);
     }
 
+
     /**
-     * @Route ("/admin/post/update/{id}", name="admin_post_update")
+     * @Route("/admin/post/update/{id}", name="admin_post_update")
      * @param int $id
      * @param Request $request
+     * @param FileManagerServiceInterface $fileManagerService
      * @return RedirectResponse|Response
      */
-    public function update(int $id, Request $request)
+    public function update(int $id, Request $request, FileManagerServiceInterface $fileManagerService)
     {
         $em = $this->getDoctrine()->getManager();
-        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        $post = $this->getDoctrine()->getRepository(Post::class)
+            ->find($id);
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            if ($form->get('save')->isClicked())
-            {
+        if ($form->isSubmitted() && $form->isValid()){
+            if ($form->get('save')->isClicked()){
+                $image = $form->get('image')->getData();
+                $imageOld = $post->getImage();
+                if ($image){
+                    if ($imageOld){
+                        $fileManagerService->removePostImage($imageOld);
+                    }
+                    $fileName = $fileManagerService->imagePostUpload($image);
+                    $post->setImage($fileName);
+                }
                 $post->setUpdateAtValue();
-                $this->addFlash('success', 'Пост изменен');
+                $this->addFlash('success', 'Пост обновлен');
             }
-            if ($form->get('delete')->isClicked())
-            {
+            if ($form->get('delete')->isClicked()){
+                $image = $post->getImage();
+                if ($image){
+                    $fileManagerService->removePostImage($image);
+                }
                 $em->remove($post);
                 $this->addFlash('success', 'Пост удален');
             }
+
             $em->flush();
             return $this->redirectToRoute('admin_post');
         }
+
         $forRender = parent::renderDefault();
-        $forRender['title'] = 'Редактирование поста';
+        $forRender['title'] = 'Редактрование поста';
         $forRender['form'] = $form->createView();
         return $this->render('admin/post/form.html.twig', $forRender);
+
     }
 }
